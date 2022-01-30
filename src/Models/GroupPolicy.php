92274e5b1b038eb7202ad2d5e6bc9229b49ddd3b
Angelinsky7 <steven.demarco@while-not-false.ch>
2022-01-30 19:56:13 +0100

    group ??

--- src/Models/GroupPolicy.php
+++ src/Models/GroupPolicy.php
@@ -8,4 +8,8 @@ namespace Darkink\AuthorizationServer\Models;
 class GroupPolicy extends Policy
 {
 
+    public function groups(){
+        return $this->belongsToMany(GroupPolicyGroup::class, 'group_policy_group', 'group_policy_id', 'group_id');
+    }
+
 }
