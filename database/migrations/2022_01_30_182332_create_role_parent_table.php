b3cc847f9da3923317c6687db689c33aeb6b847d
Angelinsky7 <steven.demarco@while-not-false.ch>
2022-01-30 19:51:22 +0100

    started to create some migration to build the new system

---
+++ database/migrations/2022_01_30_182332_create_role_parent_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoleParentTable extends Migration
+{
+    protected $schema;
+
+    public function __construct()
+    {
+        $this->schema = Schema::connection($this->getConnection());
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->schema->create('role_role', function (Blueprint $table) {
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('roles')
+                ->onDelete('cascade');
+
+            $table->unsignedBigInteger('parent_id');
+            $table->foreign('parent_id')
+                ->references('id')
+                ->on('roles')
+                ->onDelete('restrict');
+
+            $table->primary(['role_id', 'parent_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Schema::dropIfExists('role_role');
+    }
+
+    public function getConnection()
+    {
+        return config('policy.storage.database.connection');
+    }
+}
